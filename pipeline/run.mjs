5660b9e3496d502a77bd031735f6337a58896443
ValyVanDenBroeck <valy.van.den.broeck@pwc.com>
2026-04-14 19:13:11 +0200

    First Run

--- pipeline/run.mjs
+++ pipeline/run.mjs
@@ -1,10 +1,8 @@
 import path from "path";
 const origJoin = path.join;
 const origRelative = path.relative;
-const origResolve = path.resolve;
 path.join = (...args) => origJoin(...args).replaceAll("\\", "/");
 path.relative = (...args) => origRelative(...args).replaceAll("\\", "/");
-path.resolve = (...args) => origResolve(...args).replaceAll("\\", "/");
 path.sep = "/";
 
-await import("@rdfc/js-runner/bin/cli.js");
+await import("./node_modules/@rdfc/js-runner/bin/js-runner.js");
